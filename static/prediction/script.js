2b7fcc11994f78935aafa91717938f65f2c1b2f6
Bui Hoang Lan <lanbh@vng.com.vn>
2022-03-24 14:15:42 +0700

    Fix charts not display

--- static/prediction/script.js
+++ static/prediction/script.js
@@ -1,21 +1,17 @@
 const accident = document.querySelector("#accident-propability");
 const noAccident = document.querySelector("#no-accident-propability");
 
-accident.addEventListener('change', event => visualize());
-noAccident.addEventListener('change', event => visualize());
-
 const accidentPropability = parseFloat(accident.textContent);
 const noAccidentPropability = parseFloat(noAccident.textContent);
 
 const visualize = () => {
-    console.log("Hello");
     if (isNaN(accidentPropability) || isNaN(noAccidentPropability)) {
         return;
     }
     
     const predictions = [
-        {category: "Accident", "probability": accidentPropability},
-        {category: "No accident", "probability": noAccidentPropability}
+        {category: "Accident", probability: accidentPropability},
+        {category: "No accident", probability: noAccidentPropability}
     ];
     
     const cf = crossfilter(predictions);
@@ -23,14 +19,13 @@ const visualize = () => {
     
     dc.rowChart("#row-chart")
         .dimension(category)
-        .group(category.group().reduceSum(p => p.value));
+        .group(category.group().reduceSum(p => p.probability));
     
     dc.pieChart("#pie-chart")
         .dimension(category)
-        .group(category.group().reduceSum(p => p.value));
+        .group(category.group().reduceSum(p => p.probability));
     
     dc.renderAll();
 };
 
-window.onload = () => visualize();
-document.onload = () => visualize();
+visualize();
