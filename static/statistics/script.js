c5ae41b9e72d00f347b5e0908c34b7d09a23c592
Rambo Khải <kakaokoan@gmail.com>
2022-03-26 20:44:06 +0700

    Add company logo

--- static/statistics/script.js
+++ static/statistics/script.js
@@ -1,72 +1,74 @@
 // ===== Data =====
 
 const _datasets = backbone_info.map((info, index) => {
-    const r = Math.random() * 255;
-    const g = Math.random() * 255;
-    const b = Math.random() * 255;
-    const color = `rgb(${r}, ${g}, ${b})`;
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+  const color = `rgb(${r}, ${g}, ${b})`;
 
-    return {
-        label: info.name,
-        backgroundColor: color,
-        borderColor: color,
-        data: info.fpr.map((_fpr, index) => {
-            return {
-                x: _fpr,
-                y: info.tpr[index]
-            };
-        }),
-        showLine: true
-    };
+  return {
+    label: info.name,
+    backgroundColor: color,
+    borderColor: color,
+    data: info.fpr.map((_fpr, index) => {
+      return {
+        x: _fpr,
+        y: info.tpr[index],
+      };
+    }),
+    showLine: true,
+  };
 });
 
 _datasets.push({
-    label: 'No-skill',
-    backgroundColor: 'rgb(60, 60, 60)',
-    borderColor: 'rgb(60, 60, 60)',
-    data: [{
-        x: 0,
-        y: 0
-    }, {
-        x: 1,
-        y: 1
-    }],
-    showLine: true,
-    borderDash: [10,10]
+  label: "No-skill",
+  backgroundColor: "rgb(60, 60, 60)",
+  borderColor: "rgb(60, 60, 60)",
+  data: [
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      x: 1,
+      y: 1,
+    },
+  ],
+  showLine: true,
+  borderDash: [10, 10],
 });
 
 const data = {
-    datasets: _datasets
+  datasets: _datasets,
 };
 
 // ===== Options =====
 
 const options = {
-    plugins: {
-        title: {
-            display: true,
-            text: 'ROC Curves'
-        }
+  plugins: {
+    title: {
+      display: true,
+      text: "ROC Curves",
     },
     // Doesn't work
     legend: {
-        labels: {
-            filter: (legendItem, data) => legendItem.text !== 'No-skill'
-        }
-    },
+      labels: {
+        filter: (legendItem, data) => {
+          return legendItem.text !== "No-skill";
+        },
+      },
+    }
+  },
 };
 
 // ===== Config =====
 
 const config = {
-    type: 'scatter',
-    data: data,
-    options: options
+  type: "scatter",
+  data: data,
+  options: options,
 };
 
 // ===== Chart =====
 
-const myChart = new Chart(
-    document.getElementById('chart'),
-    config
-);
+const myChart = new Chart(document.getElementById("chart"), config);
